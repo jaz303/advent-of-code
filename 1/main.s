71b4e35177e2caba2a0408c4e464e081a24bab8b
Jason Frame <jason@onehackoranother.com>
2016-12-09 01:44:41 +0000

    tabs -> spaces

--- 1/main.s
+++ 1/main.s
@@ -3,52 +3,52 @@
 .text
 
 _start:
-			@ r0: current heading
-			@ 0 = north
-			@ 1 = east
-			@ 2 = south
-			@ 3 = west
-			mov r0, #0
+            @ r0: current heading
+            @ 0 = north
+            @ 1 = east
+            @ 2 = south
+            @ 3 = west
+            mov r0, #0
 
-			@ r1: data pointer
-			ldr r1, =data
-			
-			@ r2: remaining
-			ldr r8, =count
-			ldr r2, [r8]
+            @ r1: data pointer
+            ldr r1, =data
+            
+            @ r2: remaining
+            ldr r8, =count
+            ldr r2, [r8]
 
-			@ r3, r4: (x,y)
-			mov r3, #0
-			mov r4, #0
+            @ r3, r4: (x,y)
+            mov r3, #0
+            mov r4, #0
 
-			cmp r2, #0
-			b test
+            cmp r2, #0
+            b test
 
-loop:		@ update heading
-			ldr r6, [r1], #4
-			cmp r6, #0
-			addeq r0, r0, #1
-			subne r0, r0, #1
-			and r0, r0, #0x03
+loop:       @ update heading
+            ldr r6, [r1], #4
+            cmp r6, #0
+            addeq r0, r0, #1
+            subne r0, r0, #1
+            and r0, r0, #0x03
 
-			@ apply movement
-			ldr r6, [r1], #4
-			cmp r0, #0
-			addeq r4, r6
-			cmp r0, #1
-			addeq r3, r6
-			cmp r0, #2
-			subeq r4, r6
-			cmp r0, #3
-			subeq r3, r6
+            @ apply movement
+            ldr r6, [r1], #4
+            cmp r0, #0
+            addeq r4, r6
+            cmp r0, #1
+            addeq r3, r6
+            cmp r0, #2
+            subeq r4, r6
+            cmp r0, #3
+            subeq r3, r6
 
-			subs r2, r2, #1
-test:		bne loop
+            subs r2, r2, #1
+test:       bne loop
 
-			cmp r3, #0
-			rsblt r3, r3, #0
-			cmp r4, #0
-			rsblt r4, r4, #0
-			add r5, r3, r4		
+            cmp r3, #0
+            rsblt r3, r3, #0
+            cmp r4, #0
+            rsblt r4, r4, #0
+            add r5, r3, r4      
 
-stop:		b stop
+stop:       b stop
